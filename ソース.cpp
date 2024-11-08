ef57275e263ecdcba7aef979ffcb92ac849b26fc
Satori32 <Huga8989+githubf@gmail.com>
2024-11-08 18:23:53 +0900

    change spec

--- ソース.cpp
+++ ソース.cpp
@@ -10,12 +10,11 @@ struct Type {
 
 struct MemoryOne {
 	Type Info;
+	Type R;
 	//type MemoryWord = unsigned char:
 	typedef unsigned char Word;
 	Word Strage;
-	//type R = NULL;
-	typedef void* R;
-	R(*F)(Type&, Word&)=NULL;
+	//R.V(*F)(Info.V, Word&)=NULL;
 };
 
 struct MemoryStructOne {
@@ -23,14 +22,12 @@ struct MemoryStructOne {
 	typedef unsigned char T;
 	T Info;//Šæ’£‚Á‚Ä–„‚ß‚æ‚¤
 
+	Type A;
+	Type B;
+	Type R;
+
 	Vector<MemoryOne> Memory;
-	//type A = NULL;
-	//type B = NULL;
-	//type C = NULL;
-	typedef int A;
-	typedef int B;
-	typedef int C;
-	C(*F)(A&, B&)=NULL;
+	//R.V(*F)(A.V&, B.V&)=NULL;//vc cant compile it by writing time.
 };
 /** /
 struct MemoryBigSpace {
@@ -39,6 +36,11 @@ struct MemoryBigSpace {
 	T Info;//Šæ’£‚Á‚Ä–„‚ß‚æ‚¤
 
 	Vector<MemoryStructOne> Memory;
-	Vector<Type(*)(Type&, Type&)> Lamdas;
+
+	Type A;
+	Type B;
+	Type R;
+
+	Vector<R.V(*)(A.V&, B.V&)> Lamdas;
 };
 /**/
